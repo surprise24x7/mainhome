badd7fa87a06a78568c9c4cc0f8e5a5e1c99166d
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-28 20:53:54 +0530

    Update lap.js

--- lap.js
+++ lap.js
@@ -2,5 +2,5 @@
 var password = 'APPLY'
 password=prompt('SMS: Press okay to fill the application form','APPLY');
 if (password != 'APPLY') {
-location.href='https://laptop1233.blogspot.com/';
+location.href='https://zaget.cc/ads.html';
 }
