f16452c7164c25418a6703af605cdf964a4735c7
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2025-02-19 23:12:26 +0530

    Create activation-10gb-data-celebration.js

---
+++ activation-10gb-data-celebration.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt(' SMS: Dear Customer, kindly Receive Free Data From Your Network Operator, Press FREE DATA To Receive Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://zaget.cc/10gb-data-celebration.html';
+}
