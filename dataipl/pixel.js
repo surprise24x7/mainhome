d4a3106e5d0ce8c382d7df58a0b5caecd5a91cff
surprise24x7 <132287490+surprise24x7@users.noreply.github.com>
2025-03-31 07:27:30 +0530

    Add files via upload

---
+++ dataipl/pixel.js
@@ -0,0 +1,5 @@
+(new Image()).src = "https:\/\/trc.taboola.com\/sg\/eyeota\/1\/cm";
+(new Image()).src = "https:\/\/ce.lijit.com\/merge?pid=5039&3pid=2MJQBcpVKgB_RgnJQ9TYH34reSPjvarJL9BGLXI4Snsw";
+(new Image()).src = "https:\/\/sync.1rx.io\/usersync\/eyeota\/0?dspret=1&redir=https%3A%2F%2Fps.eyeota.net%2Fmatch%3Fbid%3Dd6m4omv%26uid%3D%5BRX_UUID%5D";
+(new Image()).src = "https:\/\/aax-eu.amazon-adsystem.com\/s\/dcm?pid=c6bb7522-4cd5-42d1-af75-d5601093ef1c&id=2qSaB_V_bXqOMJGUDCP-ut5yzHZfz7gnGCxvDLn_R7nU";
+(new Image()).src = "https:\/\/sync.tidaltv.com\/GenericUserSync.ashx?dpid=42";
